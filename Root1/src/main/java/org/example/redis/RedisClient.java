f6fa3181fed0f376dc9f571b6f2c03093f9a47b9
dedalusin <18374850057@163.com>
2022-07-22 10:09:03 +0800

    branch1

--- Root1/src/main/java/org/example/redis/RedisClient.java
+++ Root1/src/main/java/org/example/redis/RedisClient.java
@@ -1,2 +1,15 @@
-package org.example.redis;public class RedisClient {
+package org.example.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
+
+public class RedisClient {
+    public static final Jedis jedis = new Jedis("localhost", 6379);
+
+    public static void main(String[] args) {
+        Pipeline pipeline = jedis.pipelined();
+        pipeline.zadd("my-zset", 1, "one");
+        pipeline.zadd("my-zset", 2, "two");
+        pipeline.sync();
+    }
 }
--- Root1/src/test/java/org/example/redis/RedisTest.java
+++ Root1/src/test/java/org/example/redis/RedisTest.java
@@ -1,2 +1,17 @@
-package org.example.redis;public class RedisTest {
+package org.example.redis;
+
+import org.junit.Test;
+import redis.clients.jedis.Jedis;
+
+public class RedisTest {
+    private static final Jedis jedis = new Jedis("localhost", 6379);
+    private static final String KEY = "my-zset";
+    @Test
+    public void zsetTest() {
+//        System.out.println(jedis.zremrangeByRank(KEY, -1, -1));
+        System.out.println(jedis.zrange("my-zset", 0, -1));
+        System.out.println(jedis.zcard(KEY));
+
+    }
+
 }
